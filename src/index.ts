75a15c913db4220407628ac114533b422383b8d6
Hyeok <mh960110@gmail.com>
2020-12-31 18:23:02 +0900

    Apply pre-commit hook on ts

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,68 @@
-import * as CryptoJS from "crypto-js"
+import * as CryptoJS from "crypto-js";
 import { createNew } from "typescript";
 
 class Block {
-    public index : number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-    // it is available even if you don't create block instance
-    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string) : string => {
-        return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
-    }
+  // it is available even if you don't create block instance
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  };
 
-    constructor(index : number, hash: string, previousHash: string, data: string, timestamp: number) {
-        this.index = index;
-        this.hash = hash;
-        this.previousHash = previousHash;
-        this.data = data
-        this.timestamp = timestamp;
-    }
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-const genesisBlock:Block = new Block(0, "12321", "", "Hello", 123456);
+const genesisBlock: Block = new Block(0, "12321", "", "Hello", 123456);
 
-let blockChain: [Block] = [genesisBlock]
+let blockChain: [Block] = [genesisBlock];
 
-const getBlockChain = () : Block[] => blockChain;
+const getBlockChain = (): Block[] => blockChain;
 
-const getLastestBlock = () : Block => blockChain[blockChain.length - 1];
+const getLastestBlock = (): Block => blockChain[blockChain.length - 1];
 
-const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-const createNewBlock = (data: string) : Block => {
-    const previousBlock : Block = getLastestBlock()
-    const newIndex : number = previousBlock.index + 1;
-    const newTimeStamp : number = getNewTimeStamp();
-    const newHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data);
-    const newBlock : Block = new Block(
-        newIndex,
-        newHash,
-        previousBlock.hash,
-        data,
-        newTimeStamp
-    )
-    return newBlock
-}
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLastestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+  return newBlock;
+};
 
-console.log(createNewBlock("Hello"), createNewBlock("Byebye"))
+console.log(createNewBlock("Hello"), createNewBlock("Byebye"));
 
 export {};
