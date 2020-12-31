67e1dbc7c407dc20ecc508472f043bdb94c6b7dd
Hyeok <mh960110@gmail.com>
2020-12-31 17:49:19 +0900

    Install cryptoJS and basic functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import * as CryptoJS from "crypto-js"
+
 class Block {
     public index : number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    // it is available even if you don't create block instance
+    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string) : string => {
+        return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    }
+
     constructor(index : number, hash: string, previousHash: string, data: string, timestamp: number) {
         this.index = index;
         this.hash = hash;
@@ -16,6 +24,12 @@ class Block {
 const genesisBlock:Block = new Block(0, "12321", "", "Hello", 123456);
 
 let blockChain: [Block] = [genesisBlock]
+
+const getBlockChain = () : Block[] => blockChain;
+
+const getLastestBlock = () : Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockChain)
 
